8cd1c4f6882d1821ad2aa3acea85ffcbb71f160f
ziyidong <dongziyics@163.com>
2025-05-12 15:11:24 +0800

    add Bendmarking

---
+++ main.cpp
@@ -0,0 +1,30 @@
+﻿/* This is the bendmarking of c in miracl core
+ *
+ *author: Ziyi Dong
+*/
+
+#include <iostream>
+#include "core.h"
+
+using namespace std;
+
+int main() {
+    cout << "MIRACL core CPP Bendmarking, the elliptic curve is BLS12391" << endl;
+
+    printf("----------Point Multication on G1----------\n");
+
+
+    miracl *mip = mirsys(500, 16);
+
+
+
+
+
+
+
+
+
+    system("pause");
+
+    return 0;
+}
