ab4b83309ee663be99f740dd0c4cf778cdecd52f
ziyidong <dongziyics@163.com>
2025-05-12 15:34:57 +0800

    add .h

--- main.cpp
+++ main.cpp
@@ -5,16 +5,22 @@
 
 #include <iostream>
 #include "core.h"
+#include "bls_BLS12381.h"
 
 using namespace std;
 
 int main() {
-    cout << "MIRACL core CPP Bendmarking, the elliptic curve is BLS12391" << endl;
+    cout << "********* MIRACL core CPP Bendmarking *********" << endl;
+    cout << "*                                             *" << endl;
+    cout << "* Author: Ziyi Dong                           *" << endl;
+    cout << "* Library: lib_miraclcore_bls12381_cpp_64.a   *" << endl;
+    cout << "* Elliptic Curve: BLS12381                    *" << endl;
+    cout << "*                                             *" << endl;
+    cout << "***********************************************" << endl;
 
-    printf("----------Point Multication on G1----------\n");
+    cout << endl << "************ Point Multication on G1 **********\n" << endl;
 
-
-    miracl *mip = mirsys(500, 16);
+    //mirsys(500, 16);
 
 
 
